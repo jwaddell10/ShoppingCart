30974795f7e19216d269c65d9c145fb7ea255da7
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-16 14:42:29 -0600

    update for question

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,23 +1,12 @@
 import PropTypes from "prop-types";
 
 function Cart({ cart }) {
- console.log(cart, 'this is')
- return (
-  <>
-    {cart.length === 0 ? (
+ console.log(cart, 'this is cartitems')
+  return (
+    <>
       <h3>this cart is empty</h3>
-    ) : (
-      // Render cart items
-      <>
-        {cart.map(item => (
-          // Render individual cart items
-          // For example: <div key={item.id}>{item.name}</div>
-        ))}
-        {console.log(cart, 'this is cart')}
-      </>
-    )}
-  </>
- );
+    </>
+  );
 }
 
 Cart.propTypes = {
