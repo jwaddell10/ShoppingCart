99709989e28dad4102c93a20eb5904bc0c9cd0f8
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-16 12:06:58 -0600

    modified code to make more readable

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,11 @@ import HomePage from './components/Home.jsx';
 import Shopping from './components/Shopping.jsx';
 import Cart from './components/Cart.jsx';
 import useTitle from './components/FetchAPI.jsx';
+import PropTypes from "prop-types";
 
 function App() {
+    const { items } = useTitle();
+    console.log(items, 'this is items')
   return (
     <Router>
       <NavBar />
@@ -17,6 +20,10 @@ function App() {
       </Routes>
     </Router>
   );
+}
+
+App.propTypes = {
+    items: PropTypes.object,
 }
 
 export default App;
--- src/components/Shopping.jsx
+++ src/components/Shopping.jsx
@@ -2,7 +2,7 @@ import useTitle from "./FetchAPI.jsx";
 import PropTypes from "prop-types";
 
 const Shopping = () => {
-  const { items  } = useTitle();
+  const { items } = useTitle();
   const listItems =
     items &&
     items.map((item) => {
@@ -13,6 +13,24 @@ const Shopping = () => {
         price: item.price,
       };
     });
+
+  function DisplayItemInfo({ name, image, price, id }) {
+    return (
+      <li className="item" key={id}>
+        <h3>{name}</h3>
+        <img src={image} alt={name} height={100} width={100} />
+        <h3>${price}</h3>
+      </li>
+    );
+  }
+
+  function addToCart(name) {
+    //need to add item to cart
+    //add items properties
+    //just add name of item to cart
+    //how to trigger function in another module?
+    console.log(name, "etarget");
+  }
 
   return (
     <>
@@ -27,7 +45,7 @@ const Shopping = () => {
                 image={item.image}
                 price={item.price}
               />
-              <ManageCart name={item.name}/>
+              <button onClick={() => addToCart(item.name)}>Add To Cart</button>
             </div>
           ))}
       </ul>
@@ -35,46 +53,11 @@ const Shopping = () => {
   );
 };
 
-function DisplayItemInfo({ name, image, price, id }) {
-  return (
-    <li className="item" key={id}>
-      <h3>{name}</h3>
-      <img 
-        src={image}
-        alt={name}
-        height={100}
-        width={100}
-      />
-      <h3>${price}</h3>
-    </li>
-  );
-}
-
-function ManageCart({ name }) {
-  function addToCart() {
-    //need to add item to cart
-    //add items properties
-    //just add name of item to cart
-    //how to trigger function in another module?
-    console.log(name, 'etarget')
-  }
-  return (
-    <>
-      <button onClick={addToCart}>Add To Cart</button>
-    </>
-  );
-}
-
-DisplayItemInfo.propTypes = {
+Shopping.propTypes = {
   name: PropTypes.string,
   image: PropTypes.string,
   price: PropTypes.number,
   id: PropTypes.number,
 };
 
-ManageCart.propTypes = {
-    name: PropTypes.string,
-    price: PropTypes.number,
-}
-
-export default Shopping
+export default Shopping;
