b6d3c26cf87f7a2dd593efb221bcf1267f6d6537
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-16 15:43:11 -0600

    update, state passed to cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 function Cart({ cart }) {
- console.log(cart, 'this is cartitems')
+ const newCart = cart.find((item) => console.log(item, 'this is item'))
   return (
     <>
       <h3>this cart is empty</h3>
