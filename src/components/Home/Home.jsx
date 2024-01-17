9f1742964adacff2974ee40331703b61bb8f8972
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-17 14:40:40 -0600

    add background image to file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,3 +1,5 @@
+import styled from 'styled-components';
+
 function HomePage() {
   //needs three icons, image assets?
   //probably needs a router to the three icons
@@ -12,9 +14,23 @@ function HomePage() {
 function Body() {
   return (
     <>
-      <h1>body content here</h1>
+      <img src="src/assets/HomePageBackground.png" alt="laptopcomputerondesk"/>
     </>
   );
 }
+
+const StyledH1 = styled.h1`
+  color: #333;
+  font-size: 2em;
+  display: flex;
+`;
+
+const StyledBody = {
+  height: '100vh', // Note: Specify units for height
+  margin: 0,
+  backgroundImage: 'url("src/assets/HomePageBackground.png")', // Note: Use camelCase for backgroundImage
+  backgroundSize: 'cover', // Note: Use camelCase for backgroundSize
+  backgroundPosition: 'center', // Note: Use camelCase for backgroundPosition
+};
 
 export default HomePage;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,22 +1,41 @@
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function NavBar() {
   return (
     <nav className="navbar">
-      <h3>Website Name</h3>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/shopping">Shopping</Link>
-        </li>
-        <li>
-          <Link to="/cart">Cart</Link>
-        </li>
-      </ul>
+      <StyledNavBar>
+        <h3>Website Name</h3>
+        <ul>
+          <StyledList>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/shopping">Shopping</Link>
+            </li>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+          </StyledList>
+        </ul>
+      </StyledNavBar>
     </nav>
   );
 }
+
+const StyledNavBar = styled.section`
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  top: 0;
+  width: 100vw;
+`;
+
+const StyledList = styled.section`
+  list-style-type: none;
+  gap: 1vw;
+  display: flex;
+`;
 
 export default NavBar;
