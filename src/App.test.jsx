960a8d3578860ff54e058f63623a1d08eb793afe
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-01-23 19:25:01 -0600

    update for question

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,15 +1,4 @@
 import { describe, it, expect } from 'vitest';
-
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
-
-  it('false to be false', () => {
-    expect(false).toBe(false);
-  });
-});
-
 import { render, screen } from '@testing-library/react';
 
 import App from './App';
