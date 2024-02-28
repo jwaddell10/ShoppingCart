93268a508c17913e3d856318f7aa6cf3273dfd31
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-28 15:09:58 -0600

    added tests to render shopping component

--- src/components/Shopping/Shopping.test.jsx
+++ src/components/Shopping/Shopping.test.jsx
@@ -1,2 +1,11 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { Shopping } from "./Shopping.jsx";
 // test if item renders on page
 // test if addtocart works
+
+describe("Shopping page", () => {
+    it("renders component", () => {
+      render(<Shopping title="Shopping" />);
+    })
+  });
