ad1e47b655442dbfbc99e50a24ebaf384af328e5
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-02-28 15:05:48 -0600

    updated tests

--- vite.config.js
+++ vite.config.js
@@ -8,5 +8,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setup.js',
+    coverage: {
+      provider: 'istanbul', // or 'v8'
+      reporter: ['text', 'json', 'html'],
+    },
+    
   },
 })
