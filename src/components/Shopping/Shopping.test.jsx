2fffcb4000c85bc9d59ca182ac2ddf3c7c30dd88
jwaddell10 <115955410+jwaddell10@users.noreply.github.com>
2024-03-22 12:09:52 -0500

    tests button click in shopping cart

--- src/components/Shopping/Shopping.test.jsx
+++ src/components/Shopping/Shopping.test.jsx
@@ -1,5 +1,7 @@
+import { vi } from "vitest";
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { Shopping } from "./Shopping.jsx";
 // test if addtocart works
 
@@ -12,8 +14,28 @@ describe("Shopping page", () => {
 describe("Button click event", () => {
 	it("should render a button with the text 'Add to Cart'", () => {
 		render(<button onClick={() => {}} />);
+
 		const button = screen.getByRole("button", { name: "" });
 
 		expect(button).toBeInTheDocument();
+	});
+
+	it("should call the onClick function when clicked", async () => {
+		const onClick = vi.fn();
+		const user = userEvent.setup();
+		render(<button onClick={onClick} />);
+
+		const button = screen.getByRole("button", { name: "" });
+
+		await user.click(button);
+
+		expect(onClick).toHaveBeenCalled();
+	});
+
+	it("should not call the onClick function when it isn't clicked", async () => {
+		const onClick = vi.fn();
+		render(<button onClick={onClick} />);
+
+		expect(onClick).not.toHaveBeenCalled();
 	});
 });
